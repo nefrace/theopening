ddce7b9bd12b18340b6215648686079a0ade325e
Nefrace <nefrace@gmail.com>
2024-09-23 02:34:01 +0300

    Фиксы тайлмапы, многократный старт игры

--- Managers/SceneManager/scene_manager.gd
+++ Managers/SceneManager/scene_manager.gd
@@ -2,29 +2,43 @@ extends CanvasLayer
 
 var mat : ShaderMaterial
 
+var active := false
+
 func _ready() -> void:
 	mat = $ColorRect.material
 
 func switch_to_file(file: String, center_in: Vector2 = Vector2(80, 72), center_out: Vector2 = Vector2(80, 72)):
+	if active:
+		return
+	active = true
 	mat.set_shader_parameter("center", center_in)
 	await tween_in()
 	get_tree().change_scene_to_file(file)
 	mat.set_shader_parameter("center", center_out)
 	await tween_out()
+	active = false
 	
 func switch_to_packed(scene: PackedScene, center_in: Vector2 = Vector2(80, 72), center_out: Vector2 = Vector2(80, 72)):
+	if active:
+		return
+	active = true
 	mat.set_shader_parameter("center", center_in)
 	await tween_in()
 	get_tree().change_scene_to_packed(scene)
 	mat.set_shader_parameter("center", center_out)
 	await tween_out()
+	active = false
 	
 func restart(center_in: Vector2 = Vector2(80, 72), center_out: Vector2 = Vector2(80, 72)):
+	if active:
+		return
+	active = true
 	mat.set_shader_parameter("center", center_in)
 	await tween_in()
 	get_tree().reload_current_scene()
 	mat.set_shader_parameter("center", center_out)
 	await tween_out()
+	active = false
 
 
 func tween_in():
